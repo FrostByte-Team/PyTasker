21dc4b6aec73efcabddea7fca3b1800bb03c69e3
Bimalendu Mishra <48399921+BhimMishra@users.noreply.github.com>
2020-05-18 08:25:35 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,3 +5,8 @@ def main():
 
 if __name__ == '__main__':
   main()
+ 
+import calendar  ## Importing Module
+y = int(input("Input the year : "))  ## Input for the Year
+m = int(input("Input the month : ")) ## Input for the month
+print(calendar.month(y, m)) ## Displaying the calender. Also, month() is a built-in function taking arguments as year and month
